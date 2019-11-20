95c517665e455ac95b6394a318ee6235a42c7e1e
leos <leonardo.siri.email@gmail.com>
2019-11-20 22:59:39 +0100

    add specs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+/* EX di oggi: primo Milestone ex calendar
+
+NOME REPO:  ajax-ex-calendar
+
+DESCRIZIONE:
+trovate la descrizione  e tutto il resto qui: https://docs.google.com/document/d/1OcSGrT3Snh_DXrDZ82DVY59eqvzNb_Nh_Db5z3qq2_k/edit
+Per oggi però “solo” MIlestone:
+stampare gennaio 2018 (E SOLO QUELLO) con caratterizzazione delle relative festività, recuperate
+interrogando l’API
+
+STEP:
+Controllare quanti giorni ha il mese  formando così una lista;
+Chiedere all’api quali sono le festività per il mese;
+Evidenziare le festività nella lista */
+
 $.ajax({
     "url": "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
     "method": "GET",
